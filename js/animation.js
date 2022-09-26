29b2d14eb42be8b614122b9fabc01e58922e6cfb
Luis Montiel <93620709+MrMontiel@users.noreply.github.com>
2022-09-26 14:31:51 -0500

    cualquier cosa

---
+++ js/animation.js
@@ -0,0 +1,16 @@
+window.sr = ScrollReveal();
+    //animations for navbar 
+    sr.reveal(".linkNav",{
+        duration: 400,
+        easing: 'ease-in-out',
+        distance: '-30px'
+    })
+    sr.reveal(".reveal-proyectos",{
+        delay: 100,
+    })
+    sr.reveal(".reveal-references",{
+        delay: 200,
+    })
+    sr.reveal(".reveal-contactame",{
+        delay: 300,
+    })
--- js/main.js
+++ js/main.js
@@ -5,6 +5,13 @@ let linkNav = contentNav.getElementsByClassName("linkNav");
 
 for(let i = 0; i<linkNav.length; i++){
     linkNav[i].addEventListener("click", function(){
+        gsap.to(window, {
+            duration: 0.5, 
+            scrollTo:{
+                y:".section" + (i + 1), 
+                offsetY:70
+            }
+        });
         let current = document.getElementsByClassName("activate");
         if(current.length > 0){
             current[0].className = current[0].className.replace(" activate", "");
@@ -16,6 +23,10 @@ for(let i = 0; i<linkNav.length; i++){
 
 // Scroll - btnTop
 const btnTop = document.getElementById("btn");
+
+btnTop.addEventListener("click", ()=>{
+    gsap.to(window, {duration: 1, scrollTo:{y:"#inicio", offsetY:70}});
+})
 
 window.onscroll = () => {
     scrollFunction();
@@ -32,13 +43,38 @@ const scrollFunction = () => {
     }
 };
 
-const topFuction = () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-};
-
 // nav
 let nav = document.getElementById("nav").innerHTML
 let openNav = document.getElementById("openNav");
 
 openNav.innerHTML = nav
+
+// Form action
+const $form = document.querySelector('#formContact');
+
+$form.addEventListener("submit", handleSubmit);
+
+async function handleSubmit(event){
+    event.preventDefault();
+    const form = new FormData(this);
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    if(response.ok){
+        this.reset()
+        showSnackbar('Gracias por contactarte conmigo :)')
+    }
+}
+
+let snackbar = document.getElementById("snackbar");
+const showSnackbar = (msg) => {
+    snackbar.innerHTML = msg;
+    snackbar.classList.add("show");
+    setTimeout(() => {
+        snackbar.classList.remove("show");
+    }, 3000);
+};
